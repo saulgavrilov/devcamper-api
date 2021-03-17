016be078db8cab686032c4ef0a03a043b602758e
saul <shaulgav@gmail.com>
2021-03-17 11:26:38 +0200

    Course CRUD

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,7 +7,13 @@ const {
   deleteBootcamp,
 } = require("../controllers/bootcamps");
 
+// Include other resource routers
+const courseRouter = require("./courses");
+
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/").get(getBootcamps).post(createBootcamp);
 router.route("/:id").get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
