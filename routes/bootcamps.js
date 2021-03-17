0e2614dc597bd6396e31c8be8f600e1d7e2bbf83
saul <shaulgav@gmail.com>
2021-03-17 19:42:55 +0200

    Role Authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -16,17 +16,21 @@ const courseRouter = require("./courses");
 
 const router = express.Router();
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
 
-router.route("/:id/photo").put(protect, bootcampPhotoUpload);
+router.route("/:id/photo").put(protect, authorize("publisher", "admin"), bootcampPhotoUpload);
 
 router
   .route("/")
   .get(advancedResults(Bootcamp, "courses"), getBootcamps)
-  .post(protect, createBootcamp);
-router.route("/:id").get(getBootcamp).put(protect, updateBootcamp).delete(protect, deleteBootcamp);
+  .post(protect, authorize("publisher", "admin"), createBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(protect, authorize("publisher", "admin"), updateBootcamp)
+  .delete(protect, authorize("publisher", "admin"), deleteBootcamp);
 
 module.exports = router;
