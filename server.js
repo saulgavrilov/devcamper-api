4bd30d69452a0d87ab21fce62b6b978471f7ab97
saul <shaulgav@gmail.com>
2021-03-18 21:09:39 +0200

    v1.0.0

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -39,6 +43,23 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 75,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
