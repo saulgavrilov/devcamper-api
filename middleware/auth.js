888b19e43683ff373a484cd1aa91b1c497f26a98
saul <shaulgav@gmail.com>
2021-03-18 15:27:59 +0200

    Logout To Clear Token Cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,16 +7,18 @@ const User = require("../models/User");
 exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
+  // Set token from earer token in header
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     token = req.headers.authorization.split(" ")[1];
   }
-  // else if(req.cookies.token){
-  //   token = req.cookies.token;
-  // }
+  // Set token from cookie
+  /*else if (req.cookies.token) {
+    token = req.cookies.token;
+  }*/
 
   // Make sure token exists
   if (!token) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 
   try {
@@ -27,7 +29,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
     next();
   } catch (err) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 });
 
